38708f7e53593965811af9bcdd6ddb8246f77de1
Jason Climo <jfclimo@gmail.com>
2020-02-13 16:44:25 +1300

    Add files via upload

---
+++ precache-manifest.ca0af0d5174e9d414c0dbbc40c53f1d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2063444c7aa0ebe6abcbf077c2ad0eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f472031ce2b5b4b7a032",
+    "url": "/static/css/main.4a677e9b.chunk.css"
+  },
+  {
+    "revision": "69f36f464e3f671c6d9b",
+    "url": "/static/js/2.77408093.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.77408093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f472031ce2b5b4b7a032",
+    "url": "/static/js/main.bfbe6754.chunk.js"
+  },
+  {
+    "revision": "36dc76351af1c484da49",
+    "url": "/static/js/runtime-main.d493e20d.js"
+  }
+]);
